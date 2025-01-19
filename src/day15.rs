dba87b4e2a1c164f28ad31ff1c39e18600149eb7
Stanislav Leleka <stanislav.leleka@oracle.com>
2025-01-19 21:04:36 +0200

    filenames fix

--- src/day15.rs
+++ src/day15.rs
@@ -1,4 +1,5 @@
 use crate::utils::{in_bounds, read_lines};
+use std::{cell::RefCell, rc::Rc};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction {
@@ -41,6 +42,32 @@ impl Position {
     }
 }
 
+struct Node {
+    left: (usize, usize),
+    right: (usize, usize),
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+impl Node {
+    fn new(left: (usize, usize), right: (usize, usize)) -> Rc<Node> {
+        Rc::new(Node {
+            left,
+            right,
+            children: RefCell::new(Vec::new()),
+        })
+    }
+
+    fn add_child(&self, child: Rc<Node>) {
+        let mut children = self.children.borrow_mut();
+        if !children
+            .iter()
+            .any(|c| c.left == child.left && c.right == child.right)
+        {
+            children.push(child);
+        }
+    }
+}
+
 pub struct AocDay15 {
     pub map: Vec<Vec<char>>,
     pub directions: Vec<Direction>,
@@ -65,6 +92,17 @@ impl AocDay15 {
 
         sum_coordinates(&map, 'O')
     }
+
+    pub fn part2(&self) -> usize {
+        let mut map = resize_map(&self.map);
+        let mut robot_pos = get_robot_position(&map);
+
+        for direction in self.directions.iter() {
+            robot_pos = move_robot(&mut map, &direction, &robot_pos);
+        }
+
+        sum_coordinates(&map, '[')
+    }
 }
 
 fn move_robot(map: &mut Vec<Vec<char>>, direction: &Direction, pos: &Position) -> Position {
@@ -78,6 +116,15 @@ fn move_robot(map: &mut Vec<Vec<char>>, direction: &Direction, pos: &Position) -
             } else {
                 return pos.clone();
             }
+        }
+        '[' | ']' => {
+            let moved = shift_extended(map, pos, direction);
+            if !moved {
+                return pos.clone();
+            }
+            map[next.r][next.c] = '@';
+            map[pos.r][pos.c] = '.';
+            return next;
         }
         _ => {
             map[pos.r][pos.c] = '.';
@@ -157,6 +204,184 @@ fn shift(map: &mut Vec<Vec<char>>, direction: &Direction, r: isize, c: isize) ->
     true
 }
 
+fn shift_extended(map: &mut Vec<Vec<char>>, robot: &Position, direction: &Direction) -> bool {
+    match direction {
+        Direction::Up | Direction::Down => {
+            let root = build_tree(map, robot, direction);
+            if check_leaves_movable(map, &root, *direction) {
+                let mut old_pos = Vec::new();
+                let mut new_pos = Vec::new();
+
+                move_tree(map, &root, *direction, &mut old_pos, &mut new_pos);
+
+                for old in old_pos.iter() {
+                    map[old.0.r][old.0.c] = '.';
+                    map[old.1.r][old.1.c] = '.';
+                }
+
+                for new in new_pos.iter() {
+                    map[new.0.r][new.0.c] = '[';
+                    map[new.1.r][new.1.c] = ']';
+                }
+
+                return true;
+            }
+        }
+        Direction::Left | Direction::Right => {
+            let step = match direction {
+                Direction::Left => -1,
+                Direction::Right => 1,
+                _ => 0,
+            };
+
+            let next_pos = robot.next(direction);
+            let mut next_c = next_pos.c as isize;
+            let mut can_shift = false;
+
+            while in_bounds(map, next_pos.r as isize, next_c)
+                && map[next_pos.r][next_c as usize] != '#'
+            {
+                if map[next_pos.r][next_c as usize] == '.' {
+                    can_shift = true;
+                    break;
+                }
+                next_c += step;
+            }
+
+            if can_shift {
+                match direction {
+                    Direction::Left => {
+                        for c in next_c as usize + 1..robot.c {
+                            swap(map, *direction, robot.r as isize, c as isize);
+                        }
+                    }
+                    Direction::Right => {
+                        for c in (robot.c + 1..next_c as usize).rev() {
+                            swap(map, *direction, robot.r as isize, c as isize);
+                        }
+                    }
+                    _ => {}
+                }
+
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn build_tree(map: &Vec<Vec<char>>, robot: &Position, direction: &Direction) -> Rc<Node> {
+    let next_pos = robot.next(direction);
+    let (left, right) = get_box_position(map, next_pos.r, next_pos.c);
+
+    let node = Node::new((left.r, left.c), (right.r, right.c));
+
+    let mut queue = Vec::new();
+    queue.push(Rc::clone(&node));
+
+    while let Some(current) = queue.pop() {
+        for position in [current.left, current.right].iter() {
+            let current_pos = Position::new(position.0, position.1);
+            let next = current_pos.next(direction);
+
+            if is_box(map, next.r, next.c) {
+                let (left, right) = get_box_position(map, next.r, next.c);
+                let new_node = Node::new((left.r, left.c), (right.r, right.c));
+                current.add_child(Rc::clone(&new_node));
+                queue.push(new_node);
+            }
+        }
+    }
+
+    node
+}
+
+fn get_box_position(map: &Vec<Vec<char>>, row: usize, col: usize) -> (Position, Position) {
+    if map[row][col] == '[' {
+        return (Position::new(row, col), Position::new(row, col + 1));
+    }
+    (Position::new(row, col - 1), Position::new(row, col))
+}
+
+fn check_leaves_movable(map: &Vec<Vec<char>>, node: &Rc<Node>, direction: Direction) -> bool {
+    if !has_free_space(map, node, &direction) {
+        return false;
+    }
+
+    for child in node.children.borrow().iter() {
+        if !check_leaves_movable(map, child, direction) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn has_free_space(map: &Vec<Vec<char>>, node: &Node, direction: &Direction) -> bool {
+    let (start_r, start_c_left, start_c_right) = match direction {
+        Direction::Up => (node.left.0.saturating_sub(1), node.left.1, node.right.1),
+        Direction::Down => (node.left.0 + 1, node.left.1, node.right.1),
+        _ => return false,
+    };
+
+    (start_c_left..=start_c_right).all(|c| is_free(map, start_r, c))
+}
+
+fn move_tree(
+    map: &mut Vec<Vec<char>>,
+    node: &Rc<Node>,
+    direction: Direction,
+    old_positions: &mut Vec<(Position, Position)>,
+    new_positions: &mut Vec<(Position, Position)>,
+) {
+    for child in node.children.borrow().iter() {
+        move_tree(map, child, direction, old_positions, new_positions);
+    }
+
+    let left = Position::new(node.left.0, node.left.1).next(&direction);
+    let right = Position::new(node.right.0, node.right.1).next(&direction);
+
+    new_positions.push((left, right));
+    old_positions.push((
+        Position::new(node.left.0, node.left.1),
+        Position::new(node.right.0, node.right.1),
+    ));
+}
+
+fn is_box(map: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+    if !in_bounds(map, row as isize, col as isize) {
+        return false;
+    }
+
+    if map[row][col] == '[' || map[row][col] == ']' {
+        return true;
+    }
+
+    false
+}
+
+fn is_free(map: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+    if in_bounds(map, row as isize, col as isize) && map[row][col] != '#' {
+        return true;
+    }
+
+    false
+}
+
+fn swap(map: &mut Vec<Vec<char>>, d: Direction, r: isize, c: isize) {
+    let (new_r, new_c) = match d {
+        Direction::Left => (r, c - 1),
+        Direction::Right => (r, c + 1),
+        Direction::Up => (r - 1, c),
+        Direction::Down => (r + 1, c),
+    };
+
+    let temp = map[r as usize][c as usize];
+    map[r as usize][c as usize] = map[new_r as usize][new_c as usize];
+    map[new_r as usize][new_c as usize] = temp;
+}
+
 pub fn sum_coordinates(map: &Vec<Vec<char>>, target: char) -> usize {
     let mut sum = 0;
     for r in 0..map.len() {
@@ -180,6 +405,38 @@ pub fn get_robot_position(map: &Vec<Vec<char>>) -> Position {
     }
 
     panic!("Robot not found");
+}
+
+fn resize_map(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut new_map = vec![vec!['.'; map[0].len() * 2]; map.len()];
+
+    for r in 0..map.len() {
+        for c in 0..map[r].len() {
+            let new_c = c * 2;
+
+            if map[r][c] == '#' {
+                new_map[r][new_c] = '#';
+                new_map[r][new_c + 1] = '#';
+            }
+
+            if map[r][c] == 'O' {
+                new_map[r][new_c] = '[';
+                new_map[r][new_c + 1] = ']';
+            }
+
+            if map[r][c] == '@' {
+                new_map[r][new_c] = '@';
+                new_map[r][new_c + 1] = '.';
+            }
+
+            if map[r][c] == '.' {
+                new_map[r][new_c] = '.';
+                new_map[r][new_c + 1] = '.';
+            }
+        }
+    }
+
+    new_map
 }
 
 pub fn parse_input() -> (Vec<Vec<char>>, Vec<Direction>) {
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
-pub mod day1;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
+pub mod day08;
+pub mod day09;
 pub mod day10;
 pub mod day11;
 pub mod day12;
 pub mod day13;
 pub mod day14;
 pub mod day15;
-pub mod day15_2;
 pub mod day16;
 pub mod day17;
 pub mod day18;
 pub mod day19;
-pub mod day2;
 pub mod day20;
 pub mod day21;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
+pub mod day22;
 
 pub mod utils;
 
 fn main() {
     let day21 = day21::AocDay21::new();
-    println!("Day 21 Part 1: {}", day21.part1());
-    println!("Day 21 Part 2: {}", day21.part2());
+    println!("Part 1: {}", day21.part1());
+    println!("Part 2: {}", day21.part2());
 }
